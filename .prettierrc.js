2505f4c134104698c7a0e48a03301a5f3794d6c0
techbless <yunbin@hansung.ac.kr>
2020-04-17 01:40:59 +0900

    Add prettier setting

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'all',
+	useTabs: false,
+	tabWidth: 2,
+	printWidth: 120,
+	eslintIntegration: true,
+	formatOnSave: true,
+};
