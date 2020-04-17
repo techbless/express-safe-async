c1ff3911ca6d219d0af57345f53d41689262a8b6
techbless <yunbin@hansung.ac.kr>
2020-04-18 04:05:20 +0900

    Publish new version beta.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
-declare function wrapAsync(asyncFn: Function): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<any>;
+declare function wrapAsync(asyncFn: Function): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
 declare function AsyncHandler<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): {
     configurable: boolean;
-    get(this: T): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<any>;
+    get(this: T): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
 };
 export { wrapAsync, AsyncHandler };
 export default AsyncHandler;
