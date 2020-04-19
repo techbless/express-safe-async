5aeff00214a931bba4ddef00dfaf373d1f9d360f
techbless <yunbin@hansung.ac.kr>
2020-04-19 14:05:06 +0900

    Build library targeted ES5 for publish

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-declare function safe(asyncFn: Function): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
-declare function AsyncHandled<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): {
+export declare function safe(asyncFn: Function): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
+export declare function AsyncHandled<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): {
     configurable: boolean;
     get(this: T): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
 };
-export { safe, AsyncHandled };
 export default AsyncHandled;
