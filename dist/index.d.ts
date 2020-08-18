af6c5a61ef6921569a668203804c9316a688f45c
techbless <yunbin@hansung.ac.kr>
2020-08-19 02:36:20 +0900

    Fix d.ts file problem

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-export declare function safe(asyncFn: Function): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
-export declare function AsyncHandled<T extends Function>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<T>): {
-    configurable: boolean;
-    get(this: T): (req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>, res: Response<any>, next?: NextFunction | undefined) => Promise<any>;
+
+export declare function safe(
+  asyncFn: Function,
+): (req: Request, res: Response, next?: NextFunction | undefined) => Promise<any>;
+export declare function AsyncHandled<T extends Function>(
+  target: object,
+  propertyKey: string,
+  descriptor: TypedPropertyDescriptor<T>,
+): {
+  configurable: boolean;
+  get(this: T): (req: Request, res: Response, next?: NextFunction | undefined) => Promise<any>;
 };
 export default AsyncHandled;
